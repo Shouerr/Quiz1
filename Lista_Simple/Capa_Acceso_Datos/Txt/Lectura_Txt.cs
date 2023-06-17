3722fe227589dddd0f8df5166b296687daff936a
Shouerr <134659008+Shouerr@users.noreply.github.com>
2023-06-16 20:10:48 -0600

    Delete Lista_Simple directory

--- Lista_Simple/Capa_Acceso_Datos/Txt/Lectura_Txt.cs
+++
@@ -1,7 +0,0 @@
-﻿
-namespace Capa_Acceso_Datos.Txt
-{
-    public class Lectura_Txt
-    {
-    }
-}
--- Lista_Simple/Capa_Modelo/Nodo/Nodo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Capa_Modelo.Nodo
-{
-    public abstract class Nodo<T,N>
-    {
-        public abstract T Valor{get;set;}
-
-        public abstract N Siguiente { get; set; }
-    }
-}
